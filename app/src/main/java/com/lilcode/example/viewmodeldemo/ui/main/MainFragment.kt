833b2d0e4157ce0b71b466e9e850006c542ab1ec
NetLSS <futureprogrammer@naver.com>
2021-08-12 12:36:31 +0900

    Initial commit

---
+++ app/src/main/java/com/lilcode/example/viewmodeldemo/ui/main/MainFragment.kt
@@ -0,0 +1,52 @@
+package com.lilcode.example.viewmodeldemo.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import com.lilcode.example.viewmodeldemo.R
+import com.lilcode.example.viewmodeldemo.databinding.MainFragmentBinding
+
+class MainFragment : Fragment() {
+
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = requireNotNull(_binding)
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = MainFragmentBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        val resultObserver = Observer<Float> { result ->
+            binding.resultText.text = result.toString()
+        }
+
+        viewModel.getResult().observe(viewLifecycleOwner, resultObserver)
+
+
+        binding.convertButton.setOnClickListener {
+            if (binding.dollarText.text.isNotEmpty()) {
+                viewModel.setAmount(binding.dollarText.text.toString())
+            } else {
+                binding.resultText.text = getString(R.string.no_value)
+            }
+        }
+    }
+
+}
