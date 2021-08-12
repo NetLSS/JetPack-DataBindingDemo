2ea876e2b6173837f76b6c06460bd2ae8e848233
NetLSS <futureprogrammer@naver.com>
2021-08-12 13:20:52 +0900

    Feat: 바인딩 변수에 ViewModel 인스턴스 설정하기 (setVariable)

--- app/src/main/java/com/lilcode/example/viewmodeldemo/ui/main/MainFragment.kt
+++ app/src/main/java/com/lilcode/example/viewmodeldemo/ui/main/MainFragment.kt
@@ -1,37 +1,40 @@
 package com.lilcode.example.viewmodeldemo.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.lilcode.example.viewmodeldemo.BR.myViewModel
 import com.lilcode.example.viewmodeldemo.R
 import com.lilcode.example.viewmodeldemo.databinding.MainFragmentBinding
 
-class MainFragment : Fragment() {
 
-    private var _binding: MainFragmentBinding? = null
-    private val binding get() = requireNotNull(_binding)
+class MainFragment : Fragment() {
 
     companion object {
         fun newInstance() = MainFragment()
     }
 
     private lateinit var viewModel: MainViewModel
+    lateinit var binding: MainFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = MainFragmentBinding.inflate(layoutInflater, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false)
+        binding.lifecycleOwner = this // 프레그먼트가 존재할 때만 바인딩이 남아있어야 하기 떄문에 연결. (소멸시 같이 소멸)
         return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        binding.setVariable(myViewModel, viewModel)
     }
 
 }
